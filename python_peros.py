f2b0ec1b99c07ce552b1b5b410d5ca59c8b49096
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-30 00:04:00 +1200

    refactoring

--- python_peros.py
+++ python_peros.py
@@ -9,8 +9,19 @@ print(f'Status code: {r.status_code}')
 
 # save response
 response_dict = r.json()
+print(f'Total repositories: {response_dict["total_count"]}')
 
-# work with results
-print(response_dict.keys())
+# analyze info about repo
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+print(f'\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    # analyze first repo
+    print(f'\nName: {repo_dict["name"]}')
+    print(f'Owner: {repo_dict["owner"]["login"]}')
+    print(f'Stars: {repo_dict["stargazers_count"]}')
+    print(f'Repositories: {repo_dict["html_url"]}')
+    print(f'Description: {repo_dict["description"]}')
 
 
