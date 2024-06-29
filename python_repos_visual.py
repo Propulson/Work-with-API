c0585dff42c234186553f594b72563c2fec3b7b9
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-30 00:22:04 +1200

    refactoring our diagram

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -21,7 +21,12 @@ for repo_dict in repo_dicts:
 data = [{
     'type': 'bar',
     'x': repo_names,
-    'y': stars
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60,100,150)',
+        'line': {'width': 1.5, 'color': 'rgb(25,25,25)'}
+    },
+    'opacity': 0.6,
 }]
 my_layout = {
     'title': 'GitHub Repositories',
