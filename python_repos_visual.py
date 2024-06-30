026f49de6ba2ab1edc3a4ec7d65d106bc30bb9ab
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-30 20:23:50 +1200

    redacting

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -13,15 +13,22 @@ print(f'Total repositories: {response_dict["total_count"]}')
 
 # analyze info about repo
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, labels = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f'<a href="{repo_url}">{repo_name}</a>'
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    desc = repo_dict['description']
+    label = f'{owner}<br />{desc}'
 # create visual
 data = [{
     'type': 'bar',
-    'x': repo_names,
+    'x': repo_links,
     'y': stars,
+    'hovertext': labels,
     'marker': {
         'color': 'rgb(60,100,150)',
         'line': {'width': 1.5, 'color': 'rgb(25,25,25)'}
@@ -44,4 +51,3 @@ my_layout = {
 }
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='python-repos-chart.html')
-
